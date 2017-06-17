da4c58b2c1001a53d7bd2a26b84f74142bc75e43
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-06-18 00:52:26 +0800

    get Categories and Subcategories from database and display on index.pug

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
-import express from 'express';
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
 /* GET index page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express'
-  });
-});
+  var request = require('request')
+  var options = { method: 'GET',
+    url: 'https://gateaubread.sandbox.arcadier.io/api/consumers/categories',
+    qs: { topRows: '2' },
+    headers:
+    {
+      token: 'Qa49ztDmyUFIDafrpTg4oWj_g6Sqc390ov1vU',
+      authorization: 'Bearer Q93id07fn2_fcId9Ni8IdmlYg7cE3nikYUbnEXF0NTuYuw5XrvP',
+      'content-type': 'application/json' },
+    body: { name: 'talk', completed: false, description: 'grandma Nanny' },
+    json: true }
 
-export default router;
+  request(options, function (error, response, body) {
+    if (error) throw error
+    res.render('index', {json: body})
+  })
+})
+
+export default router
