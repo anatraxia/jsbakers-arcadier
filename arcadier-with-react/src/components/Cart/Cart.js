8001c8bf4c7df1afc06df26b0891e9293c8ec4e1
Elnathan Erh <elnathan.erh@gmail.com>
2017-07-01 14:38:23 +0800

    imported Jquery library, managed to get Json from arcadier API call

--- arcadier-with-react/src/components/Cart/Cart.js
+++ arcadier-with-react/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+/* globals React $ */
 // Import Components
 import React, { Component } from 'react';
 import Categories from '../Categories/Categories';
@@ -13,13 +14,27 @@ import uuid from 'uuid'
  * Cart
  */
 export class Cart extends Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    }
+  }
+
+  getCategories = () => {
+    var url = 'http://anyorigin.com/go?url=https://gateaubread.sandbox.arcadier.io/api/consumers/items/search&callback=?';
+    $.getJSON(url, function(data){
+      console.log(data);
+    })
+  }
 
   render() {
+    this.getCategories();
     return (
       <div className="row cartOverallDiv">
 
         <div className="col-md-4 categoriesContainer">
-          <Categories/>
+            <Categories categories={this.getCategories}/>
         </div>
 
         <div className="col-md-8 searchContainer">
